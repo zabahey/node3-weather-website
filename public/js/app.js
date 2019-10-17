6b57135e4070263acce90583bcbcb8efca1fe598
Parinya Samrongsap <parinya.s@cakeboxsolutions.com>
2019-10-17 16:29:23 +0700

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,3 @@
-console.log('Client side javascript file is loaded')
-
-// fetch('http://puzzle.mead.io/puzzle')
-// 	.then(response => {
-// 		return response.json()
-// 	})
-// 	.then(data => {
-// 		console.log(data)
-// 	})
-
 const weatherForm = document.querySelector('form')
 const searchInput = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
